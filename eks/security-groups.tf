b68ac57b2527525a16dd0790da2f0848c24935e9
smanda9 <12832756+smanda9@users.noreply.github.com>
2021-11-01 05:31:15 +0000

    adding deployment and service files

--- eks/security-groups.tf
+++ eks/security-groups.tf
@@ -4,6 +4,25 @@ resource "aws_security_group" "worker_group_mgmt_one" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  egress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
@@ -18,6 +37,24 @@ resource "aws_security_group" "worker_group_mgmt_two" {
   name_prefix = "worker_group_mgmt_two"
   vpc_id      = module.vpc.vpc_id
 
+  ingress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  egress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
   ingress {
     from_port = 22
     to_port   = 22
@@ -34,13 +71,31 @@ resource "aws_security_group" "all_worker_mgmt" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  egress {
+    from_port = 0 
+    to_port   = 0 
+    protocol  = "-1"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
 
     cidr_blocks = [
       "10.0.0.0/8",
-      "172.16.0.0/16",
+      "172.16.0.0/12",
       "192.168.0.0/16",
     ]
   }
